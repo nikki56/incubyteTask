00f04f0342ef79c4f4a37da77b1a56ef0ff224f1
Nikita <nikitadotpatel@gmail.com>
2025-03-12 01:03:06 +0530

    Created StringCalculator Component

---
+++ string-calculator-tdd/src/StringCalculator.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const StringCalculator = () => {
+  
+    return (
+        <div style={{ textAlign: "center", marginTop: "50px" }}>
+            <h2>String Calculator</h2>
+        </div>
+    );
+};
+
+export default StringCalculator;
